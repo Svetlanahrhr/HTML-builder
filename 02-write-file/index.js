a79fabe0d4e5513328621a1519f94d88ec74f022
Svetlanahrhr <kovalenkohp@mail.ru>
2022-05-20 10:10:24 +0400

    feat: and 1 2 and 3 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const { stdin, stdout } = process;
+
+
+    fs.writeFile(path.join(__dirname, 'text.txt'), 'Анкета студента 1\n', (err) => {
+        if (err) throw err;
+    });
+    stdout.write('Как тебя зовут?\n')
+    stdin.on('data', data => {
+        if (data.toString().trim() == 'exit') {
+            stdout.write('Ну и пока(\n')
+            process.exit(); 
+        } else {
+            fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+            if (err) throw err;
+            });
+            stdout.write('Напиши что нибудь? или введи exit\n')
+        }
+    });
+    
+
+
+
+
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readdir(
+  path.join(__dirname, "secret-folder"),
+  { withFileTypes: true },
+  (err, data) => {
+    if (err) throw err;
+    data.forEach((file) => {
+      fs.stat(
+        path.join(__dirname, "secret-folder", file.name),
+        (err, data1) => {
+          if (err) throw err;
+          if (file.isFile()) {
+            let [name, exten] = file.name.split(".");
+            console.log(name, " - ", exten, " - ", data1.size);
+          }
+        }
+      );
+    });
+  }
+);
