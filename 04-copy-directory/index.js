1aed740022fa0dc864af475017de32c4e4b96c61
Svetlanahrhr <maadlana@mail.ru>
2022-11-02 15:57:32 +0400

    feat: add deleting copy folder before creating

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,29 @@
-const fs = require("fs");
 const path = require("path");
+const { mkdir, copyFile, readdir, unlink } = require("fs/promises");
 
-fs.mkdir(
-  path.join(__dirname, "files-copy"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
+const pathToFolder = path.join(__dirname, "files");
+const pathToCopyFolder = path.join(__dirname, "files-copy");
 
-fs.readdir(path.join(__dirname, "files"), (err, data) => {
-  if (err) throw err;
-  data.forEach((file) => {
-    fs.copyFile(
-      path.join(__dirname, "files", file),
-      path.join(__dirname, "files-copy", file),
-      (err) => {
-        if (err) throw err;
+(async function () {
+  try {
+    await mkdir(pathToCopyFolder, { recursive: true });
+
+    const filesCopy = await readdir(pathToCopyFolder, {
+      withFileTypes: true,
+    });
+    for (const fileCopy of filesCopy) {
+      await unlink(path.join(pathToCopyFolder, fileCopy.name));
+    }
+
+    const files = await readdir(pathToFolder, {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (file.isFile()) {
+        await copyFile(path.join(pathToFolder, file.name), path.join(pathToCopyFolder, file.name));
       }
-    );
-  });
-});
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})();
