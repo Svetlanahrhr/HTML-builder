60d05ace182ca2786cd23804842e928c6c44ab8c
Svetlanahrhr <maadlana@mail.ru>
2022-11-07 21:25:14 +0400

    feat: update 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,37 +3,36 @@ const path = require("path");
 
 let res = "";
 
-fs.readdir(
-  path.join(__dirname, "styles"),
-  { withFileTypes: true },
-  (err, data) => {
-    if (err) throw err;
-    data.forEach((file) => {
-      if (file.isFile()) {
-        let [name, exten] = file.name.split(".");
-        if (exten === "css") {
-          let reader = fs.createReadStream(
-            path.join(__dirname, "styles", file.name)
-          );
-          let writer = fs.createWriteStream(
-            path.join(__dirname, "project-dist", "bundle.css")
-          );
-          const handlError = () => {
-            if (err) throw err;
-            reader.destroy();
-            writer.end("Finished with err...");
-          };
-          reader
-            .on("error", handlError)
-            .on("data", (chunk) => {
-              res += "\n";
-              res += chunk.toString();
-            })
-            .on("end", () => {
-              writer.write(res);
-            });
-        }
+(async function () {
+  let filesInFolderStyle = await fs.promises.readdir(
+    path.join(__dirname, "styles"),
+    { withFileTypes: true }
+  );
+  filesInFolderStyle.forEach((fileFromStyleFolder) => {
+    if (fileFromStyleFolder.isFile()) {
+      let exten = path.parse(fileFromStyleFolder.name).ext.slice(1);
+      if (exten === "css") {
+        let reader = fs.createReadStream(
+          path.join(__dirname, "styles", fileFromStyleFolder.name)
+        );
+        let writer = fs.createWriteStream(
+          path.join(__dirname, "project-dist", "bundle.css")
+        );
+        const handlError = (err) => {
+          if (err) throw err;
+          reader.destroy();
+          writer.end("Finished with err...");
+        };
+        reader
+          .on("error", handlError)
+          .on("data", (chunk) => {
+            res += "\n";
+            res += chunk.toString();
+          })
+          .on("end", () => {
+            writer.write(res);
+          });
       }
-    });
-  }
-);
+    }
+  });
+})();
