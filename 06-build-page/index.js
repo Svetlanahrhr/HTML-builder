8bbd2b2307b46f336cf1cbf35ff2c4a8e414083b
Svetlanahrhr <maadlana@mail.ru>
2022-11-07 22:26:46 +0400

    feat: update 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,187 +1,132 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(
-  path.join(__dirname, "project-dist"),
-  { recursive: true, force: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
+(async function () {
+  await createDir();
+  await copyAssets(
+    path.join(__dirname, "assets"),
+    path.join(__dirname, "project-dist", "assets")
+  );
+  await copyStyles();
+  await createHTMLFile();
+})();
 
-let read = fs.createReadStream(path.join(__dirname, "template.html"));
-let header = "";
-let footer = "";
-let articles = "";
-let res = "";
-let newRes = "";
-
-let readerHeader = fs.createReadStream(
-  path.join(__dirname, "components", "header.html")
-);
-let readerFooter = fs.createReadStream(
-  path.join(__dirname, "components", "footer.html")
-);
-let readerArticles = fs.createReadStream(
-  path.join(__dirname, "components", "articles.html")
-);
-
-readerHeader
-  .on("error", (err) => {
-    if (err) throw err;
-  })
-  .on("data", (chunk) => {
-    header += chunk.toString();
-  })
-  .on("end", () => {
-    //   console.log(header);
-  });
-readerFooter
-  .on("error", (err) => {
-    if (err) throw err;
-  })
-  .on("data", (chunk) => {
-    footer += chunk.toString();
-  })
-  .on("end", () => {
-    //   console.log(header);
-  });
-readerArticles
-  .on("error", (err) => {
-    if (err) throw err;
-  })
-  .on("data", (chunk) => {
-    articles += chunk.toString();
-  })
-  .on("end", () => {
-    //   console.log(header);
-  });
-
-read
-  .on("error", (err) => {
-    if (err) throw err;
-  })
-  .on("data", (chunk) => {
-    res += chunk.toString();
-  })
-  .on("end", () => {
-    newRes = res.replace(/{{footer}}/gi, footer);
-    newRes = newRes.replace(/{{articles}}/gi, articles);
-    newRes = newRes.replace(/{{header}}/gi, header);
-    // console.log(newRes);
-    let writer = fs.createWriteStream(
-      path.join(__dirname, "project-dist", "index.html")
-    );
-    writer.write(newRes);
-  });
-
-//styles
-
-let resStyles = "";
-
-fs.readdir(
-  path.join(__dirname, "styles"),
-  { withFileTypes: true },
-  (err, data) => {
-    if (err) throw err;
-    data.forEach((file) => {
-      if (file.isFile()) {
-        let [name, exten] = file.name.split(".");
-        if (exten === "css") {
-          let reader = fs.createReadStream(
-            path.join(__dirname, "styles", file.name)
-          );
-          let writer = fs.createWriteStream(
-            path.join(__dirname, "project-dist", "style.css")
-          );
-          const handlError = () => {
-            if (err) throw err;
-            reader.destroy();
-            writer.end("Finished with err...");
-          };
-          reader
-            .on("error", handlError)
-            .on("data", (chunk) => {
-              resStyles += "\n";
-              resStyles += chunk.toString();
-            })
-            .on("end", () => {
-              writer.write(resStyles);
-            });
-        }
-      }
+async function createDir() {
+  try {
+    await fs.promises.stat(path.join(__dirname, "project-dist"));
+    await fs.promises.rm(path.join(__dirname, "project-dist"), {
+      recursive: true,
     });
+  } catch (err) {
+    // console.log(err);
   }
-);
 
-//assets
-
-function creatDir(dirName) {
-  fs.mkdir(
-    path.join(__dirname, "project-dist", "assets", dirName),
-    { recursive: true, force: true },
-    (err) => {
-      if (err) throw err;
+  await fs.promises.mkdir(path.join(__dirname, "project-dist", "assets"), {
+    recursive: true,
+  });
+}
+async function copyAssets(pathToAssets, pathToNewAssets) {
+  try {
+    const files = await fs.promises.readdir(pathToAssets, {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (file.isFile()) {
+        await fs.promises.copyFile(
+          path.join(pathToAssets, file.name),
+          path.join(pathToNewAssets, file.name)
+        );
+      } else {
+        await fs.promises.mkdir(path.join(pathToNewAssets, file.name), {
+          recursive: true,
+        });
+        await copyAssets(
+          path.join(pathToAssets, file.name),
+          path.join(pathToNewAssets, file.name)
+        );
+      }
     }
+  } catch (err) {
+    console.error(err);
+  }
+}
+async function copyStyles() {
+  let res = "";
+  let filesInFolderStyle = await fs.promises.readdir(
+    path.join(__dirname, "styles"),
+    { withFileTypes: true }
+  );
+  filesInFolderStyle.forEach((fileFromStyleFolder) => {
+    if (fileFromStyleFolder.isFile()) {
+      let exten = path.parse(fileFromStyleFolder.name).ext.slice(1);
+      if (exten === "css") {
+        let reader = fs.createReadStream(
+          path.join(__dirname, "styles", fileFromStyleFolder.name)
+        );
+        let writer = fs.createWriteStream(
+          path.join(__dirname, "project-dist", "style.css")
+        );
+        const handlError = (err) => {
+          if (err) throw err;
+          reader.destroy();
+          writer.end("Finished with err...");
+        };
+        reader
+          .on("error", handlError)
+          .on("data", (chunk) => {
+            res += "\n";
+            res += chunk.toString();
+          })
+          .on("end", () => {
+            writer.write(res);
+          });
+      }
+    }
+  });
+}
+async function readData() {
+  try {
+    const files = await fs.promises.readdir(
+      path.join(__dirname, "components"),
+      {
+        withFileTypes: true,
+      }
+    );
+
+    const dataStore = {};
+
+    for (const file of files) {
+      if (file.isFile()) {
+        const data = await fs.promises.readFile(
+          path.join(path.join(__dirname, "components"), file.name),
+          "utf-8"
+        );
+        dataStore[file.name] = data;
+      }
+    }
+
+    return dataStore;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+async function createHTMLFile() {
+  const componentsData = await readData();
+
+  let templateData = await fs.promises.readFile(
+    path.join(__dirname, "template.html"),
+    "utf-8"
+  );
+
+  for (const [key, value] of Object.entries(componentsData)) {
+    templateData = templateData.replace(
+      new RegExp(`{{${path.basename(key, ".html")}}}`, "g"),
+      value
+    );
+  }
+
+  fs.promises.writeFile(
+    path.join(__dirname, "project-dist", "index.html"),
+    `${templateData}`
   );
 }
-
-fs.mkdir(
-  path.join(__dirname, "project-dist", "assets"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-fs.mkdir(
-  path.join(__dirname, "project-dist", "assets", "fonts"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-fs.mkdir(
-  path.join(__dirname, "project-dist", "assets", "img"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-fs.mkdir(
-  path.join(__dirname, "project-dist", "assets", "svg"),
-  { recursive: true },
-  (err) => {
-    if (err) throw err;
-  }
-);
-
-function readDir(readingDirName) {
-  fs.readdir(
-    path.join(__dirname, readingDirName),
-    { withFileTypes: true },
-    (err, data) => {
-      if (err) throw err;
-      data.forEach((file) => {
-        if (!file.isFile()) {
-          readingDirName += "/";
-          readingDirName += file.name;
-          readDir(readingDirName);
-          creatDir(file.name);
-        } else {
-          console.log(file.name);
-          fs.copyFile(
-            path.join(__dirname, readingDirName, file.name),
-            path.join(__dirname, "project-dist", readingDirName, file.name),
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      });
-    }
-  );
-}
-
-readDir("assets/fonts");
-readDir("assets/img");
-readDir("assets/svg");
